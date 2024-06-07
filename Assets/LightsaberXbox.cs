628cb59e6cd665db8a9d3ec48aa1a0ce69eadba2
Linus Ekberg <103569362+KR0SS0@users.noreply.github.com>
2024-06-07 16:29:20 +0200

    attacks

---
+++ Assets/LightsaberXbox.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LightsaberXbox : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private string attackTrigger1 = "attack1";
+    [SerializeField] private string attackTrigger2 = "attack2";
+    [SerializeField] private string idleTrigger = "idle";
+
+    private int attackQueue = 0;
+    private bool isAttacking = false;
+
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            QueueAttack();
+        }
+    }
+
+    private void QueueAttack()
+    {
+        if (isAttacking)
+        {
+            attackQueue = 1; // Set to 1 since only one extra attack is needed after the first
+        } else
+        {
+            StartCoroutine(PerformAttack());
+        }
+    }
+
+    private IEnumerator PerformAttack()
+    {
+        isAttacking = true;
+
+        // Play the first attack animation
+        animator.SetTrigger(attackTrigger1);
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("lightsaber_attack_1"));
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        // If another attack is queued, play the second attack animation
+        if (attackQueue > 0)
+        {
+            attackQueue = 0;
+            animator.SetTrigger(attackTrigger2);
+            yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("lightsaber_attack_2"));
+            yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        }
+
+        // Return to idle state
+        animator.SetTrigger(idleTrigger);
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("lightsaber_idle"));
+
+        isAttacking = false;
+
+        // If another attack was queued during the second attack, restart the attack sequence
+        if (attackQueue > 0)
+        {
+            QueueAttack();
+        }
+    }
+}
