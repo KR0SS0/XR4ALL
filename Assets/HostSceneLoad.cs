de765848fd879eff06348ec76ab9fc3cd3644be7
Linus Ekberg <103569362+KR0SS0@users.noreply.github.com>
2024-08-28 16:13:36 +0200

    fixes

--- Assets/HostSceneLoad.cs
+++ Assets/HostSceneLoad.cs
@@ -1,11 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class HostSceneLoad : MonoBehaviour
 {
     private static GameObject sampleInstance;
+
+    private InputAction alpha1InputAction;
+    private InputAction alpha2InputAction;
+    private InputAction alpha3InputAction;
+
     private void Awake()
     {
         if (sampleInstance != null)
@@ -15,19 +22,34 @@ public class HostSceneLoad : MonoBehaviour
         DontDestroyOnLoad(this);
     }
 
-    private void Update()
+    private void Start()
     {
-        if(Input.GetKey(KeyCode.Alpha1))
-        {
-            SceneManager.LoadScene(0);
-        } 
-        else if (Input.GetKey(KeyCode.Alpha2))
-        {
-            SceneManager.LoadScene(1);
-        } 
-        else if ( Input.GetKey(KeyCode.Alpha3))
-        {
-            SceneManager.LoadScene(2);
-        } 
+        alpha1InputAction = new InputAction(type: InputActionType.Button, binding: "<Keyboard>/1");
+        alpha2InputAction = new InputAction(type: InputActionType.Button, binding: "<Keyboard>/2");
+        alpha3InputAction = new InputAction(type: InputActionType.Button, binding: "<Keyboard>/3");
+
+        alpha1InputAction.performed += ctx => OnAlpha1Input();
+        alpha1InputAction.Enable();
+        alpha2InputAction.performed += ctx => OnAlpha2Input();
+        alpha2InputAction.Enable();
+        alpha3InputAction.performed += ctx => OnAlpha3Input();
+        alpha3InputAction.Enable();
     }
+
+    private void OnAlpha1Input()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void OnAlpha2Input()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void OnAlpha3Input()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+
 }
